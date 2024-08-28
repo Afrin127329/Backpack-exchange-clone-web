a41ad11c888432242e67d68dd909d89abd028146
Afrin Nahar <afrinnahar1999@gmail.com>
2024-08-28 12:48:44 +0600

    updated colors in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        redText: "rgb(253 75 78)",
+        blueText: "rgb(76 148 255)",
+        greenText: "rgb(0 194 120)",
+        btnPrimary: "rgb(20 21 27)",
+        baseTextHighEmphasis: "rgb(244 244 246)",
+        baseTextMedEmphasis: "rgb(150 159 175)",
+        baseTextLowEmphasis: "rgb(117 121 138)",
+        baseBorderLight: "rgb(32 33 39)",
+        baseBackgroundL0: "rgb(14 15 20)",
+        baseBackgroundL2: "rgb(32 33 39)",
+        baseBackgroundL3: "rgb(56 58 69)",
+        greenBgTransparent: "rgba(0,194,120,.08)",
       },
+      border: {},
     },
   },
   plugins: [],
