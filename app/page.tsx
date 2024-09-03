eb589c11d7dd6688e660cd39cfe4416a934c9864
Afrin Nahar <afrinnahar1999@gmail.com>
2024-09-03 20:58:57 +0600

    added dummy data on markets

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,18 @@
 
 import Hero from "@/packages/components/Hero";
 import Markets from "@/packages/components/Markets";
+import { cryptoData } from "@/packages/lib/data";
+import { CryptoData } from "@/packages/lib/type";
 
 export default function Home() {
+  const marketData: CryptoData[] = cryptoData;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2">
       <Hero />
       <div className="w-11/12 flex flex-col lg:flex-row gap-4 my-6 justify-between items-center">
-        <Markets />
-        <Markets />
-        <Markets />
+        <Markets marketData={marketData} />
+        <Markets marketData={marketData} />
+        <Markets marketData={marketData} />
       </div>
     </main>
   );
--- packages/components/Markets.tsx
+++ packages/components/Markets.tsx
@@ -1,91 +1,42 @@
 import Link from "next/link";
+import { CryptoData } from "../lib/type";
 
-const Markets = () => {
+interface MarketsProps {
+  marketData: CryptoData[];
+}
+const Markets: React.FC<MarketsProps> = ({ marketData }) => {
   return (
-    <div className="w-full lg:w-1/3 bg-white dark:bg-baseBackgroundL1 rounded-md shadow-md overflow-y-hidden">
+    <div className="w-full lg:w-1/3 bg-white dark:bg-baseBackgroundL1 rounded-t-md shadow-md overflow-y-hidden">
       <h1 className="dark:text-baseTextHighEmphasis pt-4 pl-4">Markets</h1>
       <div className="relative overflow-x-auto mt-3">
         <ul className="w-full !text-sm text-left rtl:text-right border dark:border-zinc-800 text-gray-500 dark:text-gray-400">
-          <li className="bg-white dark:bg-zinc-950 hover:bg-gray-50 dark:hover:bg-baseBackgroundL1 cursor-pointer">
-            <Link
-              href={"/trade/123"}
-              className="flex items-center justify-between"
+          {marketData.map((market) => (
+            <li
+              className="bg-white dark:bg-zinc-950 hover:bg-gray-50 dark:hover:bg-baseBackgroundL1 cursor-pointer"
+              key={market.pair}
             >
-              <span className="w-[40%] flex flex-row px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Apple MacBook
-              </span>
-              <div className="w-[30%] flex flex-row justify-end px-6 py-4">
-                Silver
-              </div>
-              <div className="w-[30%] flex flex-row justify-end px-6 py-4">
-                $2999
-              </div>
-            </Link>
-          </li>
-          <li className="bg-white dark:bg-zinc-950 hover:bg-gray-50 dark:hover:bg-baseBackgroundL1 cursor-pointer">
-            <Link
-              href={"/trade/345"}
-              className="flex items-center justify-between"
-            >
-              <span className="w-[40%] flex flex-row px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Microsoft
-              </span>
-              <div className="w-[30%] flex flex-row justify-end px-6 py-4">
-                $1999
-              </div>
-              <div className="w-[30%] flex flex-row justify-end px-6 py-4">
-                White
-              </div>
-            </Link>
-          </li>
-          <li className="bg-white dark:bg-zinc-950 hover:bg-gray-50 dark:hover:bg-baseBackgroundL1 cursor-pointer">
-            <Link
-              href={"/trade/345"}
-              className="flex items-center justify-between"
-            >
-              <span className="w-[40%]  flex flex-row px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Magic Mouse
-              </span>
-              <div className="w-[30%] flex flex-row justify-end px-6 py-4">
-                Black
-              </div>
-              <div className="w-[30%] flex flex-row justify-end px-6 py-4">
-                $99
-              </div>
-            </Link>
-          </li>
-          <li className="bg-white dark:bg-zinc-950 hover:bg-gray-50 dark:hover:bg-baseBackgroundL1 cursor-pointer">
-            <Link
-              href={"/trade/345"}
-              className="flex items-center justify-between"
-            >
-              <span className="w-[40%]  flex flex-row px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Magic Mouse
-              </span>
-              <div className="w-[30%] flex flex-row justify-end px-6 py-4">
-                Black
-              </div>
-              <div className="w-[30%] flex flex-row justify-end px-6 py-4">
-                $9999
-              </div>
-            </Link>
-          </li>
-          <li className="bg-white dark:bg-zinc-950 hover:bg-gray-50 dark:hover:bg-baseBackgroundL1 cursor-pointer">
-            <Link
-              href={"/trade/345"}
-              className="flex items-center justify-between"
-            >
-              <span className="w-[40%]  flex flex-row px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Magic 2
-              </span>
-              <div className="w-[30%] flex flex-row justify-end px-6 py-4">
-                Black
-              </div>
-              <div className="w-[30%] flex flex-row justify-end px-6 py-4">
-                $99
-              </div>
-            </Link>
-          </li>
+              <Link
+                href={`/trade/${market.pair}`}
+                className="flex items-center justify-between"
+              >
+                <span className="w-[40%] flex flex-row px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {market.pair}
+                </span>
+                <div className="w-[30%] flex flex-row justify-end px-6 py-4">
+                  {market.price}
+                </div>
+                {market.changeType === "positive" ? (
+                  <div className="w-[30%] flex flex-row justify-end px-6 py-4 text-greenText">
+                    {market.change}
+                  </div>
+                ) : (
+                  <div className="w-[30%] flex flex-row justify-end px-6 py-4 text-redText">
+                    {market.change}
+                  </div>
+                )}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
