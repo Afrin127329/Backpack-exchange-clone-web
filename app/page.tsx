9b130ed008a2d9dd3acf8d7f41398ef7533d2062
Afrin Nahar <afrinnahar1999@gmail.com>
2024-09-03 18:09:45 +0600

    hero section complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import Hero from "@/packages/components/Hero";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2">
-      <h1 className=" text-redText">Home Page</h1>
+      <Hero />
     </main>
   );
 }
---
+++ packages/components/Hero.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const Hero = () => {
+  return (
+    <div className="relative h-80 dark:text-white overflow-hidden w-11/12 rounded-lg">
+      <div className="absolute inset-0 opacity-70 darK:opacity-30">
+        <Image
+          src="/heroBg.png"
+          alt="Background Image"
+          className="object-cover object-center w-full"
+          width={900}
+          height={900}
+        />
+        <div className="absolute inset-0"></div>
+      </div>
+
+      <div className="relative z-10 flex flex-col justify-center items-start h-full p-6">
+        <h1 className="text-5xl font-bold leading-tight mb-4">
+          Refer and Earn
+        </h1>
+        <p className="text-lg text-gray-600 dark:text-baseTextHighEmphasis mb-6 dark:text-opacity-65">
+          Refer a friend and earn a percentage of their trading fees.
+        </p>
+
+        <Button className="hover:shadow-lg text-xl" size={"lg"}>
+          <Link href="/referrals">Manage Referrals</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
