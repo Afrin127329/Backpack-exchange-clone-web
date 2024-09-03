65330e9f2826bfa361bf33ab3b21e412096a8763
Afrin Nahar <afrinnahar1999@gmail.com>
2024-09-03 21:30:55 +0600

    login form UI complete

--- packages/components/Login.tsx
+++ packages/components/Login.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/packages/components/ui/card";
 import { Input } from "@/packages/components/ui/input";
 import { Label } from "@/packages/components/ui/label";
+import Image from "next/image";
 import { Icons } from "./ui/icons";
 
 export const description =
@@ -18,51 +19,61 @@ export const description =
 export const iframeHeight = "600px";
 
 export const containerClassName =
-  "w-full h-screen flex items-center justify-center px-4";
+  "w-full mt-6 flex items-center justify-center px-4";
 
 export default function LoginForm() {
   return (
-    <Card className="mx-auto max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
+    <Card className="mx-auto max-w-sm w-1/3">
+      <CardHeader className="flex flex-col justify-center items-center px-6 pt-6 pb-4">
         <CardDescription>
-          Enter your email below to login to your account
+          <Image
+            src={"/exchange-logo.png"}
+            alt="Backpack clone logo"
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
         </CardDescription>
+        <CardTitle className="text-2xl pt-4">Sign In</CardTitle>
       </CardHeader>
       <CardContent>
         <div className="grid gap-4">
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
+            <Input id="email" type="email" placeholder="Enter Email" required />
           </div>
           <div className="grid gap-2">
             <div className="flex items-center">
               <Label htmlFor="password">Password</Label>
-              <Link href="#" className="ml-auto inline-block text-sm underline">
-                Forgot your password?
-              </Link>
             </div>
-            <Input id="password" type="password" required />
+            <Input
+              id="password"
+              type="password"
+              required
+              placeholder="Enter Password"
+            />
           </div>
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
+          <Button className="w-full shadow-lg text-xl">Login</Button>
           <Button
             variant="outline"
-            className="w-full flex gap-2 items-center justify-center"
+            className="w-full shadow-lg flex gap-2 items-center justify-center"
           >
             Login with Google <Icons.google className="w-4 h-4" />
           </Button>
         </div>
-        <div className="mt-4 text-center text-sm">
-          Don&apos;t have an account?{" "}
-          <Link href="#" className="underline">
-            Sign up
+        <div className="mt-4 text-center text-xs flex justify-between items-center">
+          <span>
+            Don&apos;t have an account?{" "}
+            <Link href="/signup" className="text-blueText hover:opacity-90">
+              Sign up
+            </Link>
+          </span>
+
+          <Link
+            href="#"
+            className="ml-auto inline-block text-blueText hover:opacity-90"
+          >
+            Forgot password?
           </Link>
         </div>
       </CardContent>
