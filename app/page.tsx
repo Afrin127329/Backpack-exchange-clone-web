aeaa99ab675172b74ce1a65ea948f0347e89a499
Afrin Nahar <afrinnahar1999@gmail.com>
2024-09-03 19:40:18 +0600

    created markets layout with static data

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
 import Hero from "@/packages/components/Hero";
+import Markets from "@/packages/components/Markets";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2">
       <Hero />
+      <div className="w-11/12 flex gap-4 my-6 justify-between items-center">
+        <Markets />
+        <Markets />
+        <Markets />
+      </div>
     </main>
   );
 }
