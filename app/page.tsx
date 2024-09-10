904fbb2a7a08407b0cfc23b2f89747caf6e6d421
Afrin Nahar <afrinnahar1999@gmail.com>
2024-09-10 22:48:06 +0600

    used localStorage for showing previous response if the api response is 419

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,30 @@ import Hero from "@/packages/components/Hero";
 import { CryptoDataTable } from "@/packages/components/Tables/CryptoDataTable";
 import Markets from "@/packages/components/Tables/Markets";
 import useCoinData from "@/packages/lib/fetchData";
+import { DetailedCryptoData } from "@/packages/lib/type";
+import { useEffect, useState } from "react";
 import Loading from "./loading";
 
 export default function Home() {
+  const [coinData, setCoinData] = useState<DetailedCryptoData[] | null>();
   const { data, loading, error } = useCoinData();
+  useEffect(() => {
+    setCoinData(data);
+    if (error) {
+      const cachedData = localStorage.getItem("coinData");
+      setCoinData(cachedData ? JSON.parse(cachedData) : null);
+    }
+  }, [data, error]);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2">
       <Hero />
       <div className="w-11/12 flex flex-col lg:flex-row gap-4 my-6 justify-between items-center">
-        {data ? (
+        {coinData ? (
           <>
-            <Markets marketData={data} />
-            <Markets marketData={data} />
-            <Markets marketData={data} />
+            <Markets marketData={coinData} />
+            <Markets marketData={coinData} />
+            <Markets marketData={coinData} />
           </>
         ) : error ? (
           <div className="text-red-500 text-3xl font-bold text-center w-full">
@@ -28,8 +38,8 @@ export default function Home() {
         )}
       </div>
       <div className="w-11/12 flex flex-col lg:flex-row gap-4 my-6 justify-between items-center">
-        {data ? (
-          <CryptoDataTable data={data} />
+        {coinData ? (
+          <CryptoDataTable data={coinData} />
         ) : loading ? (
           <Loading />
         ) : (
