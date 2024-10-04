ef66ac55dc25660598b8b5cc55144a5bba28f7f7
Afrin Nahar <afrinnahar1999@gmail.com>
2024-10-04 22:28:27 +0600

    added tradingview chart via npm package

--- packages/components/TradingViewWidget.tsx
+++ packages/components/TradingViewWidget.tsx
@@ -1,51 +1,30 @@
-import { LegacyRef, memo, useEffect, useRef } from "react";
+import { LegacyRef, memo, useRef, useState } from "react";
+import { AdvancedRealTimeChart } from "react-ts-tradingview-widgets";
+
+
+
 
 function TradingViewWidget() {
   const container: any = useRef<LegacyRef<HTMLDivElement> | null>(null);
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-    script.type = "text/javascript";
-    script.async = true;
-    script.innerHTML = `
-      {
-        "width": "730",
-        "height": "550",
-        "symbol": "COINBASE:BTCUSD",
-          "interval": "60",
-          "timezone": "Etc/UTC",
-          "theme": "dark",
-          "style": "1",
-          "locale": "en",
-          "hide_side_toolbar": false,
-          "allow_symbol_change": false,
-          "calendar": false,
-          "support_host": "https://www.tradingview.com"
-      }`;
-    const containerElement = container.current;
-    if (containerElement) {
-      containerElement.appendChild(script);
-    }
+  const [hideSideBar, setHideSideBar] = useState(false);
 
-    return () => {
-      if (containerElement) {
-        containerElement.removeChild(script);
-      }
-    };
-  }, []);
+  const handleSideBar = () => {
+    const sideBarElem = document.querySelector("#drawing-toolbar")
+    console.log(sideBarElem)
+  }
 
   return (
-    <div
-      className="tradingview-widget-container min-h-screen"
-      ref={container}
-      style={{ height: "610px", width: "980px" }}
-    >
-      <div
-        className="tradingview-widget-container__widget"
-        style={{ height: "610px", width: "980px" }}
-      ></div>
+
+    <div className="w-[980px] h-[610px] bg-slate-500">
+      <button
+        className="tradingview-widget-container__close-button"
+        onClick={handleSideBar}
+      >
+        Hide
+      </button>
+      <AdvancedRealTimeChart container_id="advanced-real-time-chart" theme="dark" autosize width={980} height={610} symbol="COINBASE:BTCUSD"></AdvancedRealTimeChart>
     </div>
+
   );
 }
 
